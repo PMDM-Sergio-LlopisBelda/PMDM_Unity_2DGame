8f0b5e828c96a1859948b8ea58dafa71b2d46307
AndorBZ <andorbelszegi@gmail.com>
2024-01-31 19:36:30 +0100

    [ADD] spiderRespawn

---
+++ Assets/Scripts/Enemy/Spider/Spider.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+
+    public float droppingSpeed = 1f;
+    private Rigidbody rb;
+    private LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
